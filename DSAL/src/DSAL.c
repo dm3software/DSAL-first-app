08584f998889a0a497a8ebd7ded8db829abc2127
dm3master <dm3@Lenovo>
2018-01-30 22:13:02 +0100

    Dodałem pierwszy program

---
+++ DSAL/src/DSAL.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : DSAL.c
+ Author      : 
+ Version     :
+ Copyright   : DSAL
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello DSAL"); /* Wyświetla tekst */
+	return EXIT_SUCCESS;
+}

54df4ce90f334e42d44202d27f06ca1937a911c6
Andrzej <dm3@Lenovo>
2018-02-03 22:47:28 +0100

    Stworzenie nowej gałęzi

--- DSAL/src/DSAL.c
+++ DSAL/src/DSAL.c
@@ -15,5 +15,6 @@ int main(void) {
 	puts("Hello World"); /* Wyświetla tekst Hello World */
 	//Jakaś zmiana 2018-02-01 2243
 	//Zmiana 2018-02-01 2309
+	//Stworzenie gałęzi pierwszej
 	return EXIT_SUCCESS;
 }
